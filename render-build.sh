d3d376563c5bb4e714cd4e62f99e31fd72c95456
Karel Minarcik <k.minarcik@seznam.cz>
2025-03-19 22:03:54 +0100

    updated render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Instalace Google Chrome
-echo "Installing Google Chrome..."
-curl -sSLo google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update && apt-get install -y ./google-chrome.deb
-rm google-chrome.deb
+# Instalace Google Chrome a ChromeDriver
+apt-get update
+apt-get install -y wget curl unzip
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
 
-# Instalace ChromeDriver
-echo "Installing ChromeDriver..."
+# Stažení a instalace ChromeDriver
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}')
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-curl -sSLo chromedriver.zip "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-unzip chromedriver.zip -d /usr/local/bin/
-rm chromedriver.zip
-chmod +x /usr/local/bin/chromedriver
+wget -q "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
 
-echo "✅ Chrome & ChromeDriver installed!"
+# Oprava pro Selenium - nastavení cest
+export PATH=$PATH:/usr/bin
+
