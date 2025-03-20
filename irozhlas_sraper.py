812df9727fa66520f7ca6d8de8827cf460a8a5f7
Karel Minarcik <k.minarcik@seznam.cz>
2025-03-20 19:19:43 +0100

    testing words 'evropa', 'policie'

--- irozhlas_sraper.py
+++ irozhlas_sraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # 🔹 Klíčová slova pro filtrování článků
-KEYWORDS = ["armáda","armáda české republiky", "vojáci", "AČR", "obrana", "ministerstvo obrany", "vojenské", "zásah", "cvičení", "voják", "střelbě"]
+KEYWORDS = ["evropa", "policie","armáda","armáda české republiky", "vojáci", "AČR", "obrana", "ministerstvo obrany", "vojenské", "zásah", "cvičení", "voják", "střelbě"]
 
 def contains_keywords(text):
     """Ověří, zda text obsahuje některé z klíčových slov"""
